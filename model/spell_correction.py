19af7d8a17e3ffbda34c4e206cb22b06054a1ec1
Ayush Aggarwal <ayushaggar@gmail.com>
2019-08-22 14:47:09 +0530

    removing emoji and summarising dictionary

--- model/spell_correction.py
+++ model/spell_correction.py
@@ -14,7 +14,15 @@ def get_word(text):
     return re.findall('[a-z]+', text.lower())
 
 
+def deEmojify(inputString):
+    return inputString.encode('ascii', 'ignore').decode('ascii')
+
+
 def main(input_text, path_to_sample):
     global dictionary
+    global max_word_count
+    global total_words
     dictionary = Counter(get_word(open(path_to_sample).read()))
-
+    max_word_count = max(map(len, dictionary))
+    total_words = float(sum(dictionary.values()))
+    deEmojify(input_text)
